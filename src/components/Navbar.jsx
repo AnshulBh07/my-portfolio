4c3e59d11b673fc1943e77c297b66e8c480f2f94
Anshul Bhardwaj <anshulbh009@gmail.com>
2023-10-04 18:49:36 +0530

    -

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,9 +4,28 @@ import { LuMoonStar } from "react-icons/lu";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { CgClose } from "react-icons/cg";
 import "./navStyles.css";
+import { useEffect, useRef, useState } from "react";
 
 const Navbar = ({ sticky, darkMode, dispatch }) => {
   const styles = { color: "#fff", backgroundColor: "#212121" };
+  const [navbar, setNavbar] = useState(false);
+
+  //adding animation on view using ref and useEffect
+  const navRef = useRef(null);
+  // const btnRef = useRef([]);
+
+  useEffect(() => {
+    // btnRef.current = btnRef.current.slice(0, navItemsArray.length);
+    // const observer = new IntersectionObserver((entries) => {
+    //   entries.forEach((entry) => {
+    //     if (entry.isIntersecting) {
+    //       console.log(btnRef.current);
+    //       return;
+    //     }
+    //   });
+    // });
+    // observer.observe(navRef.current);
+  }, []);
 
   return (
     <div
@@ -14,8 +33,8 @@ const Navbar = ({ sticky, darkMode, dispatch }) => {
       style={darkMode ? styles : {}}
     >
       <p className="logo">{`<AB />`}</p>
-      <nav className="navbar">
-        <ul className="navlinks">
+      <nav className={`navbar ${navbar ? "navbar-active" : ""}`}>
+        <ul className="navlinks" ref={navRef}>
           {navItemsArray.map((item, index) => {
             return (
               <li key={index}>
@@ -50,12 +69,25 @@ const Navbar = ({ sticky, darkMode, dispatch }) => {
       </button>
 
       <div className="container__hamburger">
-        <button className="ham-btn open">
-          <RxHamburgerMenu className="ham-btn-icon" />
-        </button>
-        <button className="ham-btn close">
-          <CgClose className="ham-btn-icon" />
-        </button>
+        {!navbar && (
+          <button
+            className="ham-btn open"
+            onClick={() => {
+              setNavbar(true);
+              console.log("open");
+            }}
+          >
+            <RxHamburgerMenu
+              className="ham-btn-icon"
+              style={sticky || darkMode ? { color: "#fff" } : {}}
+            />
+          </button>
+        )}
+        {navbar && (
+          <button className="ham-btn close" onClick={() => setNavbar(false)}>
+            <CgClose className="ham-btn-icon" />
+          </button>
+        )}
       </div>
     </div>
   );
